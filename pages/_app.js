2445f9dc3c733f87bb585b71c377558752be5690
Yoyo Yang <yoyo.truly@gmail.com>
2023-02-05 19:03:58 -0500

    Wrap MUI theme to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Open Sans",
+    button: {
+      textTransform: "none",
+    },
+  },
+  palette: {
+    mode: "light",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
