49439ad0eab7c79d4fd0aed8f1f23c892d450cb2
Yoyo Yang <yoyo.truly@gmail.com>
2023-02-06 01:04:36 -0500

    Add utterance fetching

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Head from "next/head";
 import {
+  Alert,
   Box,
   Button,
   Chip,
@@ -18,7 +19,7 @@ const Item = styled(Box)(({ theme }) => ({
   padding: theme.spacing(1, 4),
 }));
 
-export default function Home() {
+export default function Home({}) {
   // Intent State --------------------
   const [intent, setIntent] = useState("reset password");
   const handleChangeIntent = (event) => {
@@ -26,13 +27,7 @@ export default function Home() {
   };
 
   // Context State --------------------
-  const [context, setContext] = useState([
-    "account",
-    "password",
-    "security",
-    "email",
-    "username",
-  ]);
+  const [context, setContext] = useState([]);
   const handleDeleteContext = (itemToDelete) => {
     setContext((prevContext) =>
       prevContext.filter((item) => item !== itemToDelete)
@@ -69,16 +64,59 @@ export default function Home() {
       />
     </Box>
   );
-  useEffect(() => {
-    console.log(context);
-  }, [context]);
 
   // Utterance State --------------------
-  const [utterances, setUtterances] = useState([
-    "Tell me more.",
-    "I want to learn more.",
-    "I want to learn more about this.",
-  ]);
+  const [utterance, setUtterance] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchUtterance = async (event, keywords) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ keywords: keywords }),
+      });
+
+      const data = await response.json();
+      setIsLoading(false);
+
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
+      }
+
+      const parseUtterance = (result) => {
+        const parsedResult = result.split(/\n\d\.\s/).slice(1);
+
+        return parsedResult.map((sentence) => {
+          if (sentence.startsWith('"') && sentence.endsWith('"')) {
+            return sentence.slice(1, -1);
+          } else {
+            return sentence;
+          }
+        });
+      };
+
+      console.log(parseUtterance(data.result));
+
+      setUtterance((prevUtterance) => [
+        ...prevUtterance,
+        ...parseUtterance(data.result),
+      ]);
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    }
+  };
 
   // Popper State --------------------
   const [anchorEl, setAnchorEl] = useState(null);
@@ -89,17 +127,6 @@ export default function Home() {
     setAnchorEl(null);
   };
   const open = Boolean(anchorEl);
-
-  // Generate Button State --------------------
-  const handleGenerateWithIntent = () => {
-    console.log(intent);
-    setAnchorEl(null);
-  };
-
-  const handleGenerateWithContext = () => {
-    console.log(context);
-    setAnchorEl(null);
-  };
 
   return (
     <>
@@ -170,14 +197,16 @@ export default function Home() {
 
             <Item sx={{ overflowY: "auto" }}>
               <Stack spacing={2}>
-                {utterances.map((utterance, index) => (
-                  <TextField
-                    key={index}
-                    size="small"
-                    defaultValue={utterance}
-                    sx={{ borderRadius: "6px" }}
-                  />
-                ))}
+                {utterance.map((item, index) => {
+                  return (
+                    <TextField
+                      key={index}
+                      size="small"
+                      defaultValue={item}
+                      sx={{ borderRadius: "6px" }}
+                    />
+                  );
+                })}
               </Stack>
             </Item>
 
@@ -186,6 +215,7 @@ export default function Home() {
                 variant="contained"
                 size="large"
                 fullWidth
+                disabled={isLoading}
                 onMouseEnter={handlePopperOpen}
                 sx={{ borderRadius: "10px" }}
               >
@@ -198,15 +228,27 @@ export default function Home() {
               >
                 <Paper elevation={24} sx={{ width: "434px" }}>
                   <Stack>
-                    <Button onClick={handleGenerateWithIntent}>
+                    <Button
+                      onClick={(event) => {
+                        const keywords = intent.split(" ").join(", ");
+                        fetchUtterance(event, keywords);
+                      }}
+                    >
                       Generate with intent only
                     </Button>
-                    <Button onClick={handleGenerateWithContext}>
+                    <Button
+                      onClick={(event) => {
+                        const keywords = context.join(", ");
+                        fetchUtterance(event, keywords);
+                      }}
+                    >
                       Generate with additional context
                     </Button>
                   </Stack>
                 </Paper>
               </Popper>
+
+              {error && <Alert severity="warning">{error}</Alert>}
             </Item>
           </Stack>
         </Paper>
@@ -214,131 +256,3 @@ export default function Home() {
     </>
   );
 }
-
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-  {
-    title: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-  { title: "The Good, the Bad and the Ugly", year: 1966 },
-  { title: "Fight Club", year: 1999 },
-  {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-  },
-  {
-    title: "Star Wars: Episode V - The Empire Strikes Back",
-    year: 1980,
-  },
-  { title: "Forrest Gump", year: 1994 },
-  { title: "Inception", year: 2010 },
-  {
-    title: "The Lord of the Rings: The Two Towers",
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: "Goodfellas", year: 1990 },
-  { title: "The Matrix", year: 1999 },
-  { title: "Seven Samurai", year: 1954 },
-  {
-    title: "Star Wars: Episode IV - A New Hope",
-    year: 1977,
-  },
-  { title: "City of God", year: 2002 },
-  { title: "Se7en", year: 1995 },
-  { title: "The Silence of the Lambs", year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: "Life Is Beautiful", year: 1997 },
-  { title: "The Usual Suspects", year: 1995 },
-  { title: "Léon: The Professional", year: 1994 },
-  { title: "Spirited Away", year: 2001 },
-  { title: "Saving Private Ryan", year: 1998 },
-  { title: "Once Upon a Time in the West", year: 1968 },
-  { title: "American History X", year: 1998 },
-  { title: "Interstellar", year: 2014 },
-  { title: "Casablanca", year: 1942 },
-  { title: "City Lights", year: 1931 },
-  { title: "Psycho", year: 1960 },
-  { title: "The Green Mile", year: 1999 },
-  { title: "The Intouchables", year: 2011 },
-  { title: "Modern Times", year: 1936 },
-  { title: "Raiders of the Lost Ark", year: 1981 },
-  { title: "Rear Window", year: 1954 },
-  { title: "The Pianist", year: 2002 },
-  { title: "The Departed", year: 2006 },
-  { title: "Terminator 2: Judgment Day", year: 1991 },
-  { title: "Back to the Future", year: 1985 },
-  { title: "Whiplash", year: 2014 },
-  { title: "Gladiator", year: 2000 },
-  { title: "Memento", year: 2000 },
-  { title: "The Prestige", year: 2006 },
-  { title: "The Lion King", year: 1994 },
-  { title: "Apocalypse Now", year: 1979 },
-  { title: "Alien", year: 1979 },
-  { title: "Sunset Boulevard", year: 1950 },
-  {
-    title:
-      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-    year: 1964,
-  },
-  { title: "The Great Dictator", year: 1940 },
-  { title: "Cinema Paradiso", year: 1988 },
-  { title: "The Lives of Others", year: 2006 },
-  { title: "Grave of the Fireflies", year: 1988 },
-  { title: "Paths of Glory", year: 1957 },
-  { title: "Django Unchained", year: 2012 },
-  { title: "The Shining", year: 1980 },
-  { title: "WALL·E", year: 2008 },
-  { title: "American Beauty", year: 1999 },
-  { title: "The Dark Knight Rises", year: 2012 },
-  { title: "Princess Mononoke", year: 1997 },
-  { title: "Aliens", year: 1986 },
-  { title: "Oldboy", year: 2003 },
-  { title: "Once Upon a Time in America", year: 1984 },
-  { title: "Witness for the Prosecution", year: 1957 },
-  { title: "Das Boot", year: 1981 },
-  { title: "Citizen Kane", year: 1941 },
-  { title: "North by Northwest", year: 1959 },
-  { title: "Vertigo", year: 1958 },
-  {
-    title: "Star Wars: Episode VI - Return of the Jedi",
-    year: 1983,
-  },
-  { title: "Reservoir Dogs", year: 1992 },
-  { title: "Braveheart", year: 1995 },
-  { title: "M", year: 1931 },
-  { title: "Requiem for a Dream", year: 2000 },
-  { title: "Amélie", year: 2001 },
-  { title: "A Clockwork Orange", year: 1971 },
-  { title: "Like Stars on Earth", year: 2007 },
-  { title: "Taxi Driver", year: 1976 },
-  { title: "Lawrence of Arabia", year: 1962 },
-  { title: "Double Indemnity", year: 1944 },
-  {
-    title: "Eternal Sunshine of the Spotless Mind",
-    year: 2004,
-  },
-  { title: "Amadeus", year: 1984 },
-  { title: "To Kill a Mockingbird", year: 1962 },
-  { title: "Toy Story 3", year: 2010 },
-  { title: "Logan", year: 2017 },
-  { title: "Full Metal Jacket", year: 1987 },
-  { title: "Dangal", year: 2016 },
-  { title: "The Sting", year: 1973 },
-  { title: "2001: A Space Odyssey", year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: "Toy Story", year: 1995 },
-  { title: "Bicycle Thieves", year: 1948 },
-  { title: "The Kid", year: 1921 },
-  { title: "Inglourious Basterds", year: 2009 },
-  { title: "Snatch", year: 2000 },
-  { title: "3 Idiots", year: 2009 },
-  { title: "Monty Python and the Holy Grail", year: 1975 },
-];
